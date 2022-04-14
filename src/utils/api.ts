42bcb9dadf2aa4649a57565f4cac4cf024678831
Mohammed Rabil <rabil@techie.com>
2022-04-14 13:16:39 +0530

    Remove comments

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,17 +3,16 @@ import axios from 'axios'
 
 const BASE_URL = 'https://taskg.herokuapp.com/api'
 
-// 1. Create an axios instance that you wish to apply the interceptor to
 export const request = axios.create({ baseURL: BASE_URL })
 
-// 2. Define token refresh function.
+
 const requestRefresh: TokenRefreshRequest = async (refreshToken: string): Promise<IAuthTokens | string> => {
   const response = await axios.post(`${BASE_URL}/token/refresh_token`, { token: refreshToken })
   return response.data.access
 }
 
 applyAuthTokenInterceptor(request, { requestRefresh })
-
+// or
 // applyAuthTokenInterceptor(request, {
 //     requestRefresh,  // async function that takes a refreshToken and returns a promise the resolves in a fresh accessToken
 //     header: "Authorization",  // header name
