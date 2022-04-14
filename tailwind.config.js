d1fd8e01cafa18151e823d9de0dd2a1c38127449
Mohammed Rabil <rabil@techie.com>
2022-04-14 08:56:33 +0530

    Configured Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  darkMode: "media",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
