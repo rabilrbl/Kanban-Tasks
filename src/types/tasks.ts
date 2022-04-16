05d01161db5ccc0ff7e72d730fded8ac95607d03
Mohammed Rabil <rabil@techie.com>
2022-04-16 10:53:24 +0530

    Update task type

--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -1,14 +1,15 @@
+export type priority = 'low' | 'medium' | 'high';
+export type status = "pending" | "in_progress" | "completed";
+
 export interface Task {
     id?: number;
     title: string;
-    priority: "low" | "medium" | "high";
+    priority: priority;
     description?: string;
     date_created ?: string;
-    status: "pending" | "in_progress" | "completed";
-    board: {
-        id: number;
-        title: string;
-    };
+    status: status;
+    board?: number;
+    board_title?: string;
 }
 
 export type TaskItem = "To Do" | "On Progress" | "Done"
