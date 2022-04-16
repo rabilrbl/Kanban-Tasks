db8a89fdc65b08eef3eeacf1f8ae0c77fdadfe95
Mohammed Rabil <rabil@techie.com>
2022-04-16 21:44:10 +0530

    Common responsive grid component

---
+++ src/components/GridFlow.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const GridFlow = (props: Props) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {props.children}
+    </div>
+  )
+}
+
+export default GridFlow
