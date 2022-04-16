af9d9080c5f4a2ae7c0c045da8f18af585d12a16
Mohammed Rabil <rabil@techie.com>
2022-04-16 10:52:02 +0530

    Improved form component

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -7,6 +7,8 @@ const FullInput = ({
   value,
   placeholder,
   onChange,
+  required,
+  options,
   ...extraProps
 }: {
   label?: string;
@@ -14,7 +16,12 @@ const FullInput = ({
   name: string;
   value?: string;
   placeholder?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  required?: boolean;
+  options?: {
+    label: string;
+    value: string;
+  }[]
+  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void;
 }) => {
   type = type ? type : "text";
   placeholder = placeholder ? placeholder : "";
@@ -25,21 +32,34 @@ const FullInput = ({
           {label}
         </label>
       )}
-      {type !== "textarea" ? <input
+      {(type === "text" && <input
         name={name}
         type={type}
         value={value}
-        className="w-full px-4 py-2 drop-shadow-lg border text-gray-800 font-medium border-none border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
+        className="w-full px-4 py-2 drop-shadow-lg text-gray-800 font-medium border-none border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
         placeholder={placeholder}
         onChange={onChange}
+        required={required}
         {...extraProps}
-      />: (
+      />) || (
+        type === "select" && <select
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        className="w-full px-4 py-2 drop-shadow-lg text-gray-800 font-medium border-none border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
+        >
+        {options?.map((op, i) => <option key={i} value={op.value}>{op.label}</option>)}
+        </select>
+      ) || (
+        type === "textarea" &&
         <textarea
           name={name}
           value={value}
-          className="w-full px-4 py-2 drop-shadow-lg border text-gray-800 font-medium border-none border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
+          className="w-full px-4 py-2 drop-shadow-lg text-gray-800 font-medium border-none border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800"
           placeholder={placeholder}
           onChange={onChange}
+          required={required}
           {...extraProps}
         />
       )}
