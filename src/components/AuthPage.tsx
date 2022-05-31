4e8e1a1b9374ff60efa8517e74a88d33385854ed
Mohammed Rabil <63334479+rabilrbl@users.noreply.github.com>
2022-05-31 21:02:47 +0530

    Fix auth page title

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -4,7 +4,7 @@ const AuthPage = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="flex flex-col justify-center items-center min-h-screen px-4 py-2 space-y-16">
-        <h1>Kanban Forms</h1>
+        <h1>Kanban Tasks</h1>
         <div className="w-full max-w-xl space-y-10">{children}</div>
       </div>
     </>
